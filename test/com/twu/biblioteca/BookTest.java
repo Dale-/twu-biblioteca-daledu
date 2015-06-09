87ecd13b9f88009a4e454030890c96e6ee8a1169
Dale <Dale@Dale.local>
2015-06-09 16:54:28 +0800

    modify testBook in BookTest

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -10,14 +10,14 @@ public class BookTest {
 
     @Before
     public void setUp() throws Exception {
-        book = new Book("Refactoring: Improving the Design of Existing Code", " Martin Fowler", "July 8, 1999");
+        book = new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", "July 8, 1999");
     }
 
     @Test
     public void testBook() throws Exception {
         assertEquals("Refactoring: Improving the Design of Existing Code", book.name);
-        assertEquals("Refactoring: Improving the Design of Existing Code", book.author);
-        assertEquals("Refactoring: Improving the Design of Existing Code", book.publishedyear);
+        assertEquals("Martin Fowler", book.author);
+        assertEquals("July 8, 1999", book.publishedyear);
     }
 
 }
