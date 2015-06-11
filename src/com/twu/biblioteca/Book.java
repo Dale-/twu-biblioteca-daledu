4d86ede1050269b8927065832a31e3340aac5fd7
Dale <18717394542@163.com>
2015-06-11 16:51:59 +0800

    diminish Book.BookLibrary and make BookTest pass

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -28,20 +28,10 @@ public class Book {
         return " BookName: " + this.name + " Author: " + this.author + " PublishedYear: " + this.publishedyear + '\n';
     }
 
-    public static Book[] BookLibrary () {
-        return new Book[]{
-                new Book("Refactoring", "Martin Fowler", "July 8, 1999"),
-                new Book("Clean Code", "Robert C. Martin ", "August 11, 2008"),
-                new Book("Head First Design Patterns", "Eric Freeman", "November 4, 2004"),
-                new Book("JavaScript: The Good Parts", " Douglas Crockford", "May, 2008"),
-                new Book("Programming in Scala", " Martin Odersky", "January 4, 2011"),
-                new Book("Head First Java", "Kathy Sierra", "February 9, 2005")};
-    }
-
     public static String listBookLibrary() {
         String BookLibraryInfo = "-----------------------------------BOOK LIBRARY-----------------------------------\n";
-        for(int i = 0; i < Book.BookLibrary().length; i++) {
-            BookLibraryInfo += "[" + (i + 1) + "]" + Book.BookLibrary()[i].getBookInfo();
+        for(int i = 0; i < BookLibrary.Books().size(); i++) {
+            BookLibraryInfo += "[" + (i + 1) + "]" + BookLibrary.Books().get(i).getBookInfo();
         }
         return BookLibraryInfo;
     }
--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -28,16 +28,10 @@ public class BookTest {
     }
 
     @Test
-    public void testBookLibrary() throws Exception {
-        assertEquals(6, Book.BookLibrary().length);
-        assertEquals("Martin Fowler", Book.BookLibrary()[0].getAuthor());
-    }
-
-    @Test
     public void testListBookLibrary() throws Exception {
         String bookLibraryInfo = "-----------------------------------BOOK LIBRARY-----------------------------------\n";
-        for(int i = 0; i < Book.BookLibrary().length; i++) {
-            bookLibraryInfo += "[" + (i + 1) + "]" + Book.BookLibrary()[i].getBookInfo();
+        for(int i = 0; i < BookLibrary.Books().size(); i++) {
+            bookLibraryInfo += "[" + (i + 1) + "]" + BookLibrary.Books().get(i).getBookInfo();
         }
         assertEquals(bookLibraryInfo, Book.listBookLibrary());
     }
