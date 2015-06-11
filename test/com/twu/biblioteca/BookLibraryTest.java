afbc241b2a9a124bac4e14905f006243c05a6aa8
Dale <18717394542@163.com>
2015-06-11 16:47:17 +0800

    modify class of BookLibrary and BookLibraryTest , in order to test pass

--- test/com/twu/biblioteca/BookLibraryTest.java
+++ test/com/twu/biblioteca/BookLibraryTest.java
@@ -1,4 +1,22 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookLibraryTest {
+
+    private BookLibrary bookLibrary;
+
+    @Before
+    public void setUp() throws Exception {
+        bookLibrary = new BookLibrary();
+    }
+
+    @Test
+    public void testBooks() {
+        assertEquals(6, BookLibrary.Books().size());
+        assertEquals("Martin Fowler", BookLibrary.Books().get(0).getAuthor());
+    }
 }
