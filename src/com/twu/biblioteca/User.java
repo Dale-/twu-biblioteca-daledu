d484ac9a3a1030b96e14b7ba8138f6930c5e3f80
Dale <18717394542@163.com>
2015-06-17 16:34:28 +0800

    add property of email and phoneNumber in the class of User

--- src/com/twu/biblioteca/User.java
+++ src/com/twu/biblioteca/User.java
@@ -1,36 +1,48 @@
 package com.twu.biblioteca;
 
-/**
- * Created by Dale on 6/10/15.
- */
 public class User {
 
     private String name;
     private String password;
     private String type;
+    private String email;
+    private String phoneNumber;
 
-    public User(String name, String password, String type) {
+    public User(String name, String password, String type, String email, String phoneNumber) {
         this.name = name;
         this.password = password;
         this.type = type;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public String getName() {
+
         return this.name;
     }
 
     public String getPassword() {
+
         return this.password;
     }
 
     public String getType() {
+
         return this.type;
     }
 
     public static User[] UserCollection() {
         return new User[]{
-                new User("Dale", "0326", "customer"),
-                new User("Dora", "0326", "librarian")
+                new User("Dale", "0326", "customer", "daledu@gmail.com", "18717394542"),
+                new User("Dora", "0326", "librarian", "dora@gmail.com", "18717394542")
         };
     }
 }
